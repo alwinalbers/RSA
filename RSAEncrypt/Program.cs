b64f183491ba88f3d48e5ce4b34d61cd9b3fc9f8
Alwin <alwinalbers@yahoo.de>
2019-03-07 12:30:26 +0100

    Fundament

--- RSAEncrypt/Program.cs
+++ RSAEncrypt/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Security.Cryptography;
 
 namespace RSAEncrypt
 {
@@ -10,6 +11,15 @@ namespace RSAEncrypt
     {
         static void Main(string[] args)
         {
+            
+            Console.WriteLine($"Text zum Verschlüsseln:");
+            string nachricht = Console.ReadLine();
+            var rsa = new RSAEnc();
+            var cypher = rsa.Encrypt(nachricht);
+
+            Console.WriteLine($"Crypted: \n{cypher}\n");            
+            Console.WriteLine($"Decrypted:\n" +rsa.Decrypt(cypher));
+
         }
     }
 }
---
+++ RSAEncrypt/RSAEnc.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace RSAEncrypt
+{
+    class RSAEnc
+    {
+        private static RSACryptoServiceProvider csp = new RSACryptoServiceProvider(2048);
+        private RSAParameters _privateKey;
+        private RSAParameters _publicKey;
+
+        public RSAEnc()
+        {
+            _privateKey = csp.ExportParameters(true);
+            _publicKey = csp.ExportParameters(false);
+        }
+
+        public string PublicKeyString()
+        {
+            var sw = new StringWriter();
+            var xs = new XmlSerializer(typeof(RSAParameters));
+            xs.Serialize(sw, _publicKey);
+            return sw.ToString();
+        }
+
+        public string Encrypt (string text)
+        {
+            csp = new RSACryptoServiceProvider();
+            csp.ImportParameters(_publicKey);
+
+            var data = Encoding.Unicode.GetBytes(text);
+            var cypher = csp.Encrypt(data, true);
+
+            return Convert.ToBase64String(cypher);
+        }
+
+        public string Decrypt (string cypherText)
+        {
+            var dataBytes = Convert.FromBase64String(cypherText);
+            csp.ImportParameters(_privateKey);
+            var plainText = csp.Decrypt(dataBytes, true);
+            return Encoding.Unicode.GetString(plainText);
+        }
+
+
+    }
+}
